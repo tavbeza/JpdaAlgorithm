e61f06d8031b5044e56af8e84e02fb9af511ebd2
tavbeza <49399501+tavbeza@users.noreply.github.com>
2019-11-11 16:11:45 +0200

    SrvDspMath Class

---
+++ SrvDspMath.h
@@ -0,0 +1,23 @@
+#pragma once
+class SrvDspMath
+{
+public:
+
+	/// <summary>
+	/// Default SrvDspMath constructor
+	/// </summary>
+	SrvDspMath();
+	
+	/// <summary>
+	/// Destructors
+	/// </summary>
+	~SrvDspMath();
+
+	/// <summary>
+	/// Complex base e exponential 
+	/// </summary>
+	double exp(double x);
+
+
+};
+
